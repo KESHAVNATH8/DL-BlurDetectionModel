9ffeaac992a0bafc4484a2bbddd3d2c66743b94e
keshav87 <keshavnathc@gmail.com>
2023-12-22 15:18:20 +0530

    changes added

---
+++ main.py
@@ -0,0 +1,7 @@
+from BlurImageclassification import logger
+
+logger.info("welcome to my custom log")
+
+# D:\BLUR.CLEAR\DL-BlurDetectionModel\src\BlurImageclassification\__init__.py
+# D:\BLUR.CLEAR\DL-BlurDetectionModel\src
+# D:\BLUR.CLEAR\DL-BlurDetectionModel\src\BlurImageclassification
